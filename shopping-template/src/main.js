2b5dbcf949f2ccaa38b4eb6cb45f3a3bb123aa79
shj <hajong1206@gmail.com>
2024-01-02 20:00:19 +0900

    display item

--- shopping-template/src/main.js
+++ shopping-template/src/main.js
@@ -4,8 +4,25 @@ const loadItems = () => {
     .then((json) => {return json.items})
 }
 
+const displayItems = (items) => {
+    const container = document.querySelector(".items");
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+const createHTMLString = (item) => {
+    return (
+    `
+        <li class="item">
+            <img src="${item.image}" alt="" class="item__thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+    `
+    )
+}
+
 loadItems()
     .then((items) => {
         console.log(items)
+        displayItems(items)
     })
     .catch(err => console.log(err))
