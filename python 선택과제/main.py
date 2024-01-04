43e0db47c0fcd708315074a8aef3bacd8fa03cd5
shj <hajong1206@gmail.com>
2024-01-05 02:29:55 +0900

    br1-3

--- python 선택과제/main.py
+++ python 선택과제/main.py
@@ -1,5 +1,22 @@
 
 num = 0
 
-a = int(input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :'))
-print(a)
+while(True):
+
+    a = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+
+    try:
+        int(a)
+        it_is = True
+    except ValueError:
+        it_is = False
+
+    if(it_is == True):
+        if(int(a) == 1 or int(a) == 2 or int(a) == 3):
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+    else:
+        print('정수를 입력하세요')
+
+    
